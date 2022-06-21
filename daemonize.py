547ac0df98b85d8db0ccba7a7c94f7cee3b8d1e8
YoungHoonJun <junyh2@naver.com>
2022-06-21 13:45:26 +0000

    220621

---
+++ daemonize.py
@@ -0,0 +1,72 @@
+import os                                                                                                 
+import sys
+import time
+import logging
+import signal
+
+import argparse
+
+class CollectLog:
+    def __init__(self, log_file=None):
+        logging.basicConfig(level=logging.INFO, format='%(message)s')
+        self.logger = logging.getLogger("CollectLog")
+        self.log_file = log_file
+        self.__stop = False
+
+    def main(self):
+        self.logger.info("Start, PID {0}".format(os.getpid()))
+
+        file_string = "/workspace/nvidia-examples/cnn/nvutils/log" + str(hvd_rank)  + ".txt"
+
+        while not self.__stop:
+            f_read = open(file_string, "r")
+            f_read.write("@@@@@@@@@")
+            f_read.close()
+            time.sleep(0.5)
+
+        return 0
+
+    def stop(self, signum, frame):
+        self.__stop = True
+        self.logger.info("Receive Signal {0}".format(signum))
+        self.logger.info("Stop")
+
+def run():
+    collect_log = CollectLog()
+    exit_code = collect_log.main()
+    exit(exit_code)
+
+def daemonize():
+    # double fork, first fork
+    pid = os.fork()
+    if pid > 0:
+        exit(0)
+    else:
+        # decouple from parent envronment
+        os.chdir('/')
+        os.setsid()
+        os.umask(0)
+
+        # second fork
+        pid = os.fork()
+        if pid > 0:
+            exit(0)
+        else:
+            sys.stdout.flush()
+            sys.stderr.flush()
+
+            si = open(os.devnull, 'r')
+            so = open(os.devnull, 'a+')
+            se = open(os.devnull, 'a+')
+
+            os.dup2(si.fileno(), sys.stdin.fileno())
+            os.dup2(so.fileno(), sys.stdout.fileno())
+            os.dup2(se.fileno(), sys.stderr.fileno())
+
+            with open("/workspace/nvidia-examples/cnn/nvutils/daemon_pid.pid", "w") as pid_file:
+                pid_file.write(str(os.getpid()))
+
+            run()
+
+if __name__ == '__main__':
+    daemonize()
