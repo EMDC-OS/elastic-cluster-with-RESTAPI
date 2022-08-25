a04261a16af8ed73051c81cf0b43aa3c96267ba4
YoungHoonJun <junyh2@naver.com>
2022-08-25 06:38:05 +0000

    220825

--- flaskpkg/flask_main.py
+++ flaskpkg/flask_main.py
@@ -311,5 +311,5 @@ def update():
         'gpu_log': log_list_string,
     })
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, debug=True)
+#if __name__ == "__main__":
+#    app.run(host="0.0.0.0", port=8080, debug=True)
